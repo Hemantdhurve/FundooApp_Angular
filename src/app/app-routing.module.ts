b28321513d6afb0a7146d5bd76d4165362f4f5de
Hemantdhurve <hemantdhurve007@gmail.com>
2023-02-14 21:30:35 +0530

    [Hemant] Added components and design pages of dashboard and display notes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DisplaynotesComponent } from './components/displaynotes/displaynotes.component';
+import { IconsComponent } from './components/icons/icons.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 
@@ -7,6 +10,13 @@ const routes: Routes = [
   {path:'',redirectTo:'/login',pathMatch:'full'},
   {path:'login',component:LoginComponent},
   {path:'registration',component:RegistrationComponent},
+  {path:'dashboard',component:DashboardComponent,
+  children:[
+    {path:'displaynotes',component:DisplaynotesComponent}
+  ]
+},
+{path:'icons',component:IconsComponent},
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,12 +14,22 @@ import {FormsModule} from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 import { HttpClientModule } from '@angular/common/http';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import { DisplaynotesComponent } from './components/displaynotes/displaynotes.component';
+import { IconsComponent } from './components/icons/icons.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    DashboardComponent,
+    DisplaynotesComponent,
+    IconsComponent
   ],
   imports: [
     BrowserModule,
@@ -32,8 +42,11 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
     FormsModule,
     MatButtonModule,
     HttpClientModule,
-    MatCheckboxModule
-    
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
